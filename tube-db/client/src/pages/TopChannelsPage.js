d7086820b9aa635c529f20bbee822331c779217f
DerekTaylorUPenn <88289764+DerekTaylorUPenn@users.noreply.github.com>
2022-07-27 20:40:20 -0400

    Small formatting change TopChannels

--- tube-db/client/src/pages/TopChannelsPage.js
+++ tube-db/client/src/pages/TopChannelsPage.js
@@ -21,7 +21,7 @@ function numFormatter(num) {
     }else if(num >= 1000000000){
         return (num/1000000000).toFixed(1) + 'B'; // convert to B where num >= 1 bil
     }else if(num < 900){
-        return num; // if value < 1000, nothing to do
+        return num; // if num < 1000, do nothing
     }
 }
 
