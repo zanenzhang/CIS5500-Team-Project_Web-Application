b7772658f8f2e18ba076d48991d636216f7d2825
Zanenzhang <zanzhang@seas.upenn.edu>
2022-07-31 14:57:53 -0400

    Added more elements into the headerbar

--- tube-db/client/src/components/HeaderBar.js
+++ tube-db/client/src/components/HeaderBar.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Grid.css';
+import './HeaderBar.css';
 import HeaderLogo from '../components/HeaderLogo';
 import SearchBar from '../components/SearchBar';
+import { DatePicker, Space } from 'antd';
+import { Form, FormInput, FormGroup, Button, Card, CardBody, CardTitle, Progress } from "shards-react";
+import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
 import {
     Table,
     Pagination,
-    Select
+    Select,
+    Slider
   } from 'antd'
 
 const { Column, ColumnGroup } = Table;
 const { Option } = Select;
+const { RangePicker } = DatePicker;
+
+const IconSlider = (props) => {
+const { max, min } = props;
+const [value, setValue] = useState(0);
+const mid = Number(((max - min) / 2).toFixed(5));
+const preColorCls = value >= mid ? '' : 'icon-wrapper-active';
+const nextColorCls = value >= mid ? 'icon-wrapper-active' : '';
+return (
+    <div className="icon-wrapper">
+    <FrownOutlined className={preColorCls} />
+    <Slider {...props} onChange={setValue} value={value} />
+    <SmileOutlined className={nextColorCls} />
+    </div>
+);
+};
 
 
 class HeaderBar extends React.Component {
@@ -36,6 +57,10 @@ class HeaderBar extends React.Component {
                 <Option value="UnitedKingdom">United Kingdom</Option>
                 <Option value="UnitedStates">United States</Option>
               </Select>
+              <RangePicker style={{width: 250}} />
+              <div id="slider" style={{width: 250}}>
+              <IconSlider min={0} max={20} />;
+              </div>
             </div>
           </div>
         )
