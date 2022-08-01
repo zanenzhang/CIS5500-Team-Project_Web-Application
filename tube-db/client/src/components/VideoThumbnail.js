ac0c83feaa9ed92bad1f22d2d0d54960838a1cc8
Zanenzhang <zanzhang@seas.upenn.edu>
2022-08-01 02:04:36 -0400

    Video is rendering on videopage

--- tube-db/client/src/components/VideoThumbnail.js
+++ tube-db/client/src/components/VideoThumbnail.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import './VideoThumbnail.css';
 
 const VideoThumbnail = ({thumbLink, videoId}) => (
-  <div>
-      <Link to={`/video?videoid=${videoId}`}>
-        <img className="thumbnail" src={thumbLink} alt='video-thumb' />
-      </Link>
-  </div>
+  
+  <Link to={`/video?videoid=${videoId}`}>
+    <img className="thumbnail" src={thumbLink} alt='video-thumb' />
+  </Link>
+    
 );
 
 export default VideoThumbnail;
--- tube-db/client/src/pages/VideoPage.js
+++ tube-db/client/src/pages/VideoPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SideMenu from '../components/SideMenu';
 import './HomePage.css'
+import './VideoPage.css'
 import { getHomeVideos, getSingleVideo } from '../fetcher'
 import HeaderBar from '../components/HeaderBar';
 import Grid from '../components/Grid';
 import VideoThumbnail from '../components/VideoThumbnail';
+import { useParams } from 'react-router-dom';
 
 import {
   Table,
@@ -17,26 +19,34 @@ const { Option } = Select;
 
 class VideoPage extends React.Component {
 
-  constructor(props) {
-    super(props)
+      state = {
+        fullLink: "",
+        videoInfo: [],
+        videoId: ""
+      };
 
-      this.state = {
-        videoInfo: []
-      }
-    }
+    fetchVideoId = async () => {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const videoId = urlParams.get("videoid");
+      this.state.videoId = videoId;
+      var linkBegin = "https://www.youtube.com/embed/";
+      this.state.fullLink = linkBegin + `${this.state.videoId}`;
+    };
   
     componentDidMount() {
+      this.fetchVideoId();
       getSingleVideo(this.state.videoId).then(res => {
         this.setState({ videoInfo: res.results });
       })
     };
+    
   
     render() {
   
       return (
         
         <div>
-  
             <HeaderBar />
   
             <div id="page">
@@ -47,8 +57,11 @@ class VideoPage extends React.Component {
                     </div>
                 </div>
             
-                <div id="pageContent">
-                video details here
+                <div className="videoInfo">
+
+                  <iframe width="640" height="360" 
+                      src={this.state.fullLink}>
+                  </iframe>
     
                 </div>
             
