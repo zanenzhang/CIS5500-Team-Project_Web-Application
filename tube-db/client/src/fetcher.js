cf024aecb1995f97e5cf44e328e394907a6c87be
frederickqi <tairanqi.seas.upenn.edu>
2022-08-11 08:43:23 -0700

    Updated the favorite page and added a recommendation system

--- tube-db/client/src/fetcher.js
+++ tube-db/client/src/fetcher.js
@@ -50,8 +50,8 @@ const getTrendingVideos = async (country, channelLanguage, pageCount, offset, tr
     return final
 }
 
-const getFavoritedVideos = async (user) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/favorited?user=${user}`, {
+const getFavoritedVideos = async (videoid) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/favorited?videoid=${videoid}`, {
         method: 'GET',
     })
 
